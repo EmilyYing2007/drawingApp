eb39bb91e247904c8b26323801acd30a90ca96f5
Emily Ying <emily.ying1978@gmail.com>
2026-04-16 02:01:26 -0400

    mediapipe, opencv, cmu graphics
    installed mediapipe and opencv and cmu graphics

---
+++ jbhv nm,.py
@@ -0,0 +1,15 @@
+''' Include a triple-quoted string at the top of 
+your code that briefly describes the key features
+that you want us to grade. If it is not obvious,
+you also need to include brief instructions for 
+graders explaining how to make those features visible.
+'''
+import cv2 as cv
+import mediapipe as mp
+from cmu_graphics import *
+print('emily is the best')
+
+def redrawAll(app):
+    drawRect(100, 100, 200, 200, fill='red')
+
+runApp()
