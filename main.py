d13bc06d602791b57eef3964170f1b08524d3fd8
Emily Ying <emily.ying1978@gmail.com>
2026-04-17 12:01:37 -0400

    Webcam and hand tracking

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ you also need to include brief instructions for
 graders explaining how to make those features visible.
 '''
 import cv2 as cv
+import numpy as np
 import mediapipe as mp
+from mediapipe.tasks.python import vision
+
 from cmu_graphics import *
 print('emily is the best')
+
+
+
+def onAppStart(app):
+    app.width = 1000
+    app.height = 1000
 
 def redrawAll(app):
     drawRect(100, 100, 200, 200, fill='red')
